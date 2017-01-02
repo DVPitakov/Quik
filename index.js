6566e97e76863311b89c6abf6286b3fffa87cc12
dvpitakov <dvpitakov@gmail.com>
2017-01-03 00:48:32 +0300

    удалил лишние файлы

--- index.js
+++
@@ -1,9 +0,0 @@
-var server = require("./server");
-var router = require("./router");
-var requestHandlers = require("./requestHandlers");
-
-var handle = {}
-handle["/db/api/user/create/"] = requestHandlers.userCreate;
-console.log(handle);
-server.start(router.route, handle);
-
